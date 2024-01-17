dc51360faba54004b70b7594f59a0c84d94a2bc8
Shiva Pokhrel <69740754+ShivaPokhrel@users.noreply.github.com>
2024-01-17 12:20:35 +1100

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.t8_blockchain_food_nutrition_freshwhile_20230525;
 
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "produce.db";
@@ -12,12 +14,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    
     @Override
     public void onCreate(SQLiteDatabase db) {
         createProduceTable(db);
         createUserTable(db);
     }
 
+    
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS produce");
@@ -35,6 +39,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(createTableQuery);
     }
 
+    
     private void createUserTable(SQLiteDatabase db) {
         String createTableQuery = "CREATE TABLE users ("
                 + "user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
